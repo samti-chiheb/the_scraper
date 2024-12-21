0654ff9c248097e6a67af22207f9b2c09cfcd198
chihebgit <chiheb.samti@gmail.com>
2024-12-21 01:02:12 +0100

    init project & add layout & add sidebar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Separator } from "@/components/ui/separator";
+import DesktopSidebar from "@/components/Sidebar";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex h-screen">
+      <DesktopSidebar />
+      <div className="flex flex-col flex-1 min-h-screen">
+        <header className="flex items-center justify-between px-6 py-4 h-[50px] container ">
+          the scraper
+        </header>
+        <Separator />
+        <div className="overflow-auto">
+          <div className="flex-1 container py-4 text-accent-foreground">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default layout;
