5e83899cbab84f3ad0fc83911b4576b2a2fd85eb
chihebgit <chiheb.samti@gmail.com>
2024-12-23 22:26:12 +0100

    add mobile sidebar

--- components/BreadcrumbHeader.tsx
+++ components/BreadcrumbHeader.tsx
@@ -8,6 +8,7 @@ import {
   BreadcrumbList,
   BreadcrumbSeparator,
 } from "./ui/breadcrumb";
+import { MobileSidebar } from "./Sidebar";
 
 const BreadcrumbHeader = () => {
   const pathname = usePathname();
@@ -15,6 +16,7 @@ const BreadcrumbHeader = () => {
 
   return (
     <div className="flex items-center flex-start">
+      <MobileSidebar />
       <Breadcrumb>
         <BreadcrumbList>
           {paths.map((path, index) => (
