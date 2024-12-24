4ad0216c2be30246637061334a2d1197f876dda9
chihebgit <chiheb.samti@gmail.com>
2024-12-24 01:05:35 +0100

    add user button

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,6 +3,7 @@ import { Separator } from "@/components/ui/separator";
 import DesktopSidebar from "@/components/Sidebar";
 import BreadcrumbHeader from "@/components/BreadcrumbHeader";
 import ThemeModeToggle from "../../components/ThemeModeToggle";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -13,6 +14,9 @@ const layout = ({ children }: { children: React.ReactNode }) => {
           <BreadcrumbHeader />
           <div className="gap-1 flex items-center">
             <ThemeModeToggle />
+            <SignedIn>
+              <UserButton/>
+            </SignedIn>
           </div>
         </header>
         <Separator />
